7f5628d195276408e8568ac64d87da2e1f5b016a
joevegacoding <josephbouhanef@gmail.com>
2022-03-08 23:58:02 -0500

    initial set up for the script

---
+++ content.js
@@ -0,0 +1,41 @@
+let theRockImages = [
+  "https://media.allure.com/photos/6154778ae59dcf29244cc558/3:4/w_2310,h_3080,c_limit/Dwayne%20The%20Rock%20Johnson%20Nail%20Art%20Lede.jpg",
+  "https://m.media-amazon.com/images/M/MV5BMTkyNDQ3NzAxM15BMl5BanBnXkFtZTgwODIwMTQ0NTE@._V1_UY1200_CR84,0,630,1200_AL_.jpg",
+  "https://www.google.com/url?sa=i&url=https%3A%2F%2Fknowyourmeme.com%2Fmemes%2Fpeople%2Fdwayne-the-rock-johnson%2Fphotos&psig=AOvVaw0zjgoxW5g7LH5q2MOG54i9&ust=1646886005703000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNDi596WuPYCFQAAAAAdAAAAABAD",
+  "https://freepikpsd.com/file/2019/11/dwayne-johnson-face-png-Transparent-Images-235x300.png",
+  "https://i.imgur.com/tmVJtna.jpeg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRi3eWZbtIYmjxjPZFI2LtAODLRfwVmcwhH5Q&usqp=CAU",
+  "https://i.insider.com/60fec36017cf960019ee17d1?width=1136&format=jpeg",
+  "https://i.kym-cdn.com/entries/icons/facebook/000/018/124/therock.jpg",
+  "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.robotbutt.com%2F2018%2F06%2F14%2Fthe-story-of-my-dwayne-the-rock-johnson-facial-reconstructive-surgery%2F&psig=AOvVaw2HfqUyy7sxHQdylNZpPFym&ust=1646886337051000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMihluuXuPYCFQAAAAAdAAAAABAP",
+  "https://see.news/wp-content/uploads/2021/06/dwayne-johnson-actor-smile-face-wallpaper-preview.jpg",
+];
+
+const text = "if you smellllll!!!"
+
+const texts = document.get
+
+const images = document.getElementsByTagName("img");
+
+// images.map(image => {
+//     const randomImage = Math.floor(Math.random() * theRockImages.length);
+//     image.src = theRockImages[randomImage];
+// })
+
+for (let i = 0; i < images.length; i++) {
+    const randomImage = Math.floor(Math.random() * theRockImages.length);
+    images[i].src = theRockImages[randomImage];
+}
+
+
+const header = document.getElementsByTagName("h1");
+const header2 = document.getElementsByTagName("h2");
+const header3 = document.getElementsByTagName("h3");
+const header4 = document.getElementsByTagName("p");
+const header5 = document.getElementsByTagName("a");
+
+
+for (let i = 0; i < images.length; i++) {
+
+   
+}
